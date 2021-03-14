81e36cf9c8094ab1e8d13263f56e1b0bd56cd2a9
Cristy <mikayla-grace@urban-warrior.org>
2021-03-14 15:48:30 -0400

    ...

---
+++ MagickCache/magick-baseconfig.h
@@ -0,0 +1,39 @@
+#ifndef _MAGICKCACHE_MAGICK_BASECONFIG_H
+#define _MAGICKCACHE_MAGICK_BASECONFIG_H 1
+ 
+/* MagickCache/magick-baseconfig.h. Generated automatically at end of configure. */
+/* config/config.h.  Generated from config.h.in by configure.  */
+/* config/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to the address where bug reports for this package should be sent. */
+#ifndef MAGICKCACHE_PACKAGE_BUGREPORT
+#define MAGICKCACHE_PACKAGE_BUGREPORT "https://github.com/ImageMagick/MagickCache/issues"
+#endif
+
+/* Define to the full name of this package. */
+#ifndef MAGICKCACHE_PACKAGE_NAME
+#define MAGICKCACHE_PACKAGE_NAME "MagickCache"
+#endif
+
+/* Define to the full name and version of this package. */
+#ifndef MAGICKCACHE_PACKAGE_STRING
+#define MAGICKCACHE_PACKAGE_STRING "MagickCache 1.0.0-0"
+#endif
+
+/* Define to the one symbol short name of this package. */
+#ifndef MAGICKCACHE_PACKAGE_TARNAME
+#define MAGICKCACHE_PACKAGE_TARNAME "MagickCache"
+#endif
+
+/* Define to the home page for this package. */
+#ifndef MAGICKCACHE_PACKAGE_URL
+#define MAGICKCACHE_PACKAGE_URL "https://imagemagick.org"
+#endif
+
+/* Define to the version of this package. */
+#ifndef MAGICKCACHE_PACKAGE_VERSION
+#define MAGICKCACHE_PACKAGE_VERSION "1.0.0-0"
+#endif
+ 
+/* once: _MAGICKCACHE_MAGICK_BASECONFIG_H */
+#endif

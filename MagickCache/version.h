e9291a4db3809df4607d454508c681b74bcd6440
Cristy <urban-warrior@imagemagick.org>
2023-04-30 19:01:33 -0400

    latest automake / autoconf

--- MagickCache/version.h
+++ MagickCache/version.h
@@ -33,7 +33,7 @@ extern "C" {
 #define MagickCacheLibAddendum  "-0"
 #define MagickCacheLibInterface  0
 #define MagickCacheLibMinInterface  0
-#define MagickCacheReleaseDate  "2023-04-29"
+#define MagickCacheReleaseDate  "2023-04-30"
 #define MagickCacheAuthoritativeURL  "http://www.imagemagick.org"
 #define MagickCacheVersion MagickCachePackageName " " MagickCacheLibVersionText \
   MagickCacheLibAddendum " " MagickCacheReleaseDate " " MagickCacheAuthoritativeURL
